3247380fe51b9d5b9e4643df152008b553295fd3
DevLeoCraft <leonardoantoniosilva7700@gmail.com>
2026-05-08 19:10:04 -0300

    fix: refactor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-// Criação de uma instância do Axios com a URL base configurada para o backend, facilitando as requisições HTTP para os endpoints da API. Essa instância pode ser importada e utilizada em diferentes partes da aplicação para realizar operações como GET, POST, PUT e DELETE, garantindo uma comunicação consistente com o servidor.
+// O Vite utiliza 'import.meta.env' para aceder a variáveis de ambiente.
+
+const baseURL = import.meta.env.VITE_API_URL;
+
 export const api = axios.create({
-  baseURL: 'https://siapesq-api-j101.onrender.com',
+  baseURL,
 });
