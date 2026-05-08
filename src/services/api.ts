d1923638d2df11113ad3cd243ec4f75bad00d797
DevLeoCraft <leonardoantoniosilva7700@gmail.com>
2026-05-07 22:14:59 -0300

    feat: atualizar a URL base da instância do Axios para o ambiente de produção

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 // Criação de uma instância do Axios com a URL base configurada para o backend, facilitando as requisições HTTP para os endpoints da API. Essa instância pode ser importada e utilizada em diferentes partes da aplicação para realizar operações como GET, POST, PUT e DELETE, garantindo uma comunicação consistente com o servidor.
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://siapesq-api-j101.onrender.com',
 });
